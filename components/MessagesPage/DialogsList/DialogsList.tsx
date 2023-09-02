bcad6a8323e1b2e7c3dfc4345c499ea550932119
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-09-02 19:41:25 +0300

    Style minor fixes

--- components/MessagesPage/DialogsList/DialogsList.tsx
+++ components/MessagesPage/DialogsList/DialogsList.tsx
@@ -11,10 +11,12 @@ export default function DialogsList({users}: DialogsListProps): React.ReactNode 
     return (
         <div className={styles.dialogs_list}>
             {users.map(user =>
-                <DialogLinkContainer
-                    key={user.id}
-                    user={user}
-                />
+                <div className={styles.link_wrapper}>
+                    <DialogLinkContainer
+                        key={user.id}
+                        user={user}
+                    />
+                </div>
             )}
         </div>
     )
